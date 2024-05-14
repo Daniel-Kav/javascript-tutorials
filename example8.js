0e11755a27716222e20289c88da1ffce740e3efc
daniel-kav <dankavatha01@gmail.com>
2024-05-14 16:00:43 +0300

    reversed seven star patern

---
+++ example8.js
@@ -0,0 +1,14 @@
+for(let i =0 ; i < 1; i++) {
+    let line = '';
+    for(let j = 0 ; j <7; j++) {
+        line += '*';
+    }
+    console.log(line);
+}
+for(let i= 0; i <7; i++) {
+    let line = '';
+    for(let j = 0; j <1; j++) {
+        line += '*';
+    }
+    console.log(line);
+}
