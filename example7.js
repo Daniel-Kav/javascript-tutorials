e805a88af64b0e6a5d63cba854324656b02ffcc7
daniel-kav <dankavatha01@gmail.com>
2024-05-14 15:54:32 +0300

    seven star pattern

---
+++ example7.js
@@ -0,0 +1,15 @@
+for(let i =  1; i <= 1; i++) {
+    let line = '';
+    for (let j = 0; j < 7 ; j++) {
+        line += '*';
+    }
+    console.log(line)
+}
+for(let i = 0; i <7; i++) {
+    let line = '';
+    for (let j = 0; j < 6 ; j++){
+        line += ' ';
+    }
+    line += '*';
+    console.log(line)
+}
