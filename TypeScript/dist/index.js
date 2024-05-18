c879d6ee62561384b9548714b5c5e09f33592d0a
daniel-kav <dankavatha01@gmail.com>
2024-05-18 19:40:49 +0300

    manipulating dom using ts

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -1,12 +1,8 @@
-let id : string = 'hello';
-
+"use strict";
+let id = 'hello';
 // id = 4
-
 console.log(id);
-
 let bodyT = document.getElementsByTagName('body');
-
 let div = document.createElement('div');
 div.innerHTML = "<strong>Typescript is great</strong>";
-
 document.body.appendChild(div);
